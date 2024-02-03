4bd0823e2b2e783823fb5b1c2bf61cc25fff426e
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-03 18:26:22 -0300

    limpeza

--- create.php
+++ create.php
@@ -3,7 +3,7 @@
     include_once("templates/header.php");
 
 ?>
-    <h1>Criando contato</h1>
+
 <?php
 
     include_once("templates/footer.php");
--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 
 ?>
 
-    <h1>Testando Agenda</h1>
-    <i class="fas fa-eye"></i>
-
 <?php
 
     include_once("templates/footer.php");
