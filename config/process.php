2a54afd7eca547a97d13924a9e1e60bf295637bc
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-04 18:23:31 -0300

    função de editar pronta

--- config/process.php
+++ config/process.php
@@ -28,6 +28,30 @@
                 $error = $exception->getMessage();
                 echo "Error: $error";
             }
+        } else if ($data["type"] === "edit"){
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = "UPDATE contacts " .
+            "SET name = :name, phone = :phone, observations = :observations " .
+            "WHERE id = :id";
+
+            $stmt = $connection->prepare($query);
+
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try{
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato Editado Com Sucesso!";
+            } catch(PDOException $exception){
+                $error = $exception->getMessage();
+                echo "Error: $error";
+            }
         }
         header("Location:" . $BASE_URL . "../index.php");
     } else {
