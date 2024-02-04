f7e999d64f48b39e04e4d6499e95255541b8b9b3
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-04 17:15:39 -0300

    visualizar um contato está funcionando agora

--- config/process.php
+++ config/process.php
@@ -5,10 +5,27 @@
     include_once("db_connection.php");
     include_once("url.php");
 
+
+    $id;
+    if(!empty($_GET)){
+        //retorna um contato
+        $id = $_GET["id"];
+    }
+
+    if(!empty($id)){
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $connection->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contact = $stmt->fetch();
+    } else {
+            //retorna todos os contatos
     $query = "SELECT * FROM contacts";
 
     $stmt = $connection->prepare($query);
     $stmt -> execute();
     $contacts = $stmt->fetchAll();
-    
+
+    }
+
 ?>
